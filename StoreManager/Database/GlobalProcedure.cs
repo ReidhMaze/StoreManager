ef7bdb850ec67bb271bbe31be5662a58fa53f6f9
Joseph Tristan Subong <tristansubong@gmail.com>
2024-10-19 21:26:23 +0800

    fix: change pass

--- StoreManager/Database/GlobalProcedure.cs
+++ StoreManager/Database/GlobalProcedure.cs
@@ -47,7 +47,7 @@ namespace LaundrySystem
                 servername = "localhost";
                 databasename = "store_manager_db";
                 username = "root";
-                password = "";
+                password = "bajed";
                 port = "3306";
 
                 strConnection = "Server=" + servername + ";" +
